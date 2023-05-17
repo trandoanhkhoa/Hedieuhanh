8a31e87668a8a33f6160153909314f8a1ffdfe95
Anh Khoa <trandoanhkhoa192837@gmail.com>
2023-05-17 23:02:16 +0700

    thay doi code

--- hdh.cpp
+++ hdh.cpp
@@ -2,24 +2,27 @@
 #include <queue>
 #include <algorithm>
 using namespace std;
+
 struct Tientrinh {
-    int id;   // Tên tiến trình
-    int tgden;// Thời gian đến
-    int tgxl; //Thời gian xử lý
-    int tght; //Thời gian hoàn thành
-    int tgxv; //Thời gian xoay vòng
-    int tgph; //Thời gian phản hồi
-    int priority; 
+    int id;    // Tên tiến trình
+    int tgden; // Thời gian đến
+    int tgxl;  // Thời gian xử lý
+    int tght;  // Thời gian hoàn thành
+    int tgxv;  // Thời gian xoay vòng
+    int tgph;  // Thời gian phản hồi
 };
+
 void LaplichCPU(queue<Tientrinh>& processes, int sltt) {
     int tghientai = 0;
     int tongtgcho = 0;
     int totaltgxv = 0;
     int totaltgph = 0;
+    int tongtgchay = 0; // Tổng thời gian chạy của tất cả các tiến trình
+
     while (!processes.empty()) {
         Tientrinh tthientai = processes.front();
         processes.pop();
-        cout << "Qúa trình chạy của tiến trình " << tthientai.id << " từ " << tghientai << " đến " << tghientai + tthientai.tgxl << endl;
+        cout << "Quá trình chạy của tiến trình " << tthientai.id << " từ " << tghientai << " đến " << tghientai + tthientai.tgxl << endl;
         int waitingTime = tghientai - tthientai.tgden;
         tongtgcho += waitingTime;
         tghientai += tthientai.tgxl;
@@ -27,38 +30,70 @@ void LaplichCPU(queue<Tientrinh>& processes, int sltt) {
         totaltgxv += tthientai.tgxv;
         tthientai.tgph = tghientai - tthientai.tgden - tthientai.tgxl;
         totaltgph += tthientai.tgph;
+        tongtgchay += tthientai.tgxl;
     }
-    double tgchotb = (tongtgcho*1.0)/sltt;
-    double averagetgxv = (totaltgxv*1.0)/sltt;
-    double averagetgph = (totaltgph*1.0)/sltt;
+
+    double tgchotb = (tongtgcho * 1.0) / sltt;
+    double averagetgxv = (totaltgxv * 1.0) / sltt;
+    double averagetgph = (totaltgph * 1.0) / sltt;
+    double cpus = ((tongtgchay * 1.0) / tghientai) * 100; // Tỷ lệ sử dụng CPU
+
     cout << "Thời gian chờ trung bình: " << tgchotb << endl;
     cout << "Thời gian xoay vòng trung bình: " << averagetgxv << endl;
     cout << "Thời gian phản hồi trung bình: " << averagetgph << endl;
+    cout << "Hiệu suất CPU: " << cpus << "%" << endl;
 }
- 
+
 int main() {
     queue<Tientrinh> tientrinh;
-    tientrinh.push({1, 2, 7, 0, 0}); 
-    tientrinh.push({2, 4, 1, 0, 0}); 
-    tientrinh.push({3, 2, 4, 0, 0}); 
-    tientrinh.push({4, 0, 4, 0, 0}); 
-    //Đẩy tất cả các tiến trình vào một mảng vector
+    int n; // Số lượng tiến trình
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        Tientrinh t;
+        t.id = i + 1; // Tăng id tự động
+        cin >> t.tgden >> t.tgxl;
+        tientrinh.push(t);
+    }
+
     vector<Tientrinh> sxtientrinh;
     while (!tientrinh.empty()) {
         sxtientrinh.push_back(tientrinh.front());
         tientrinh.pop();
     }
-    //Sắp xếp theo thời gian đến và thời gian xử lý tiến trình
+
+    // Sắp xếp theo thời gian đến và thời gian xử lý tiến trình
     sort(sxtientrinh.begin(), sxtientrinh.end(), [](const Tientrinh& p1, const Tientrinh& p2) {
-        if (p1.tgden != p2.tgden) {
-            return p1.tgden < p2.tgden;
-        } else {
+        if (p1.tgxl != p2.tgxl) {
             return p1.tgxl < p2.tgxl;
+        } else {
+            return p1.tgden < p2.tgden;
         }
     });
-    for (const auto& tt : sxtientrinh) {
-            tientrinh.push(tt);
+
+    Tientrinh tmp;
+    int vt = 0;
+    int ID = -1;
+    int tg = 9999;
+    for (int i = 0; i < sxtientrinh.size(); i++) {
+        if (sxtientrinh[i].tgden < tg) {
+            ID = sxtientrinh[i].id;
+            tg = sxtientrinh[i].tgden;
+            tmp = sxtientrinh[i];
+            vt = i;
+        }
     }
+
+    // Xóa đi tiến trình chạy đầu tiên trong mảng vector
+    sxtientrinh.erase(sxtientrinh.begin() + vt);
+
+    // Đẩy tiến trình đầu tiên vào hàng đợi để chạy
+    tientrinh.push(tmp);
+
+    for (const auto& tt : sxtientrinh) {
+        tientrinh.push(tt);
+    }
+
     LaplichCPU(tientrinh, tientrinh.size());
+
     return 0;
-} 
+}
